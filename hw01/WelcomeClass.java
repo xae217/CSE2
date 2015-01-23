c0c8ea6d542f4bf3614a6a007aa4ee0922917fd6
Xavier Escobar <xavier_18_96@hotmail.com>
2015-01-23 19:29:54 +0000

    homework 1

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,14 @@
+public class WelcomeClass{
+    
+        public static void main(String[] args) {
+            System.out.println("  -----------");
+            System.out.println("  | WELCOME |");
+            System.out.println("  -----------");
+            System.out.println("  ^  ^  ^  ^  ^  ^  ");
+            System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+            System.out.println("<-X--A--E--2--1--7->");
+            System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+            System.out.println("  v  v  v  v  v  v  ");
+            System.out.println("My name is Xavier, I was born in Ecuador and moved to the United States 5 years ago.");
+        }
+}
