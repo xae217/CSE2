6cdcf0ffcd9451bd9067eb941cd4c7268f5ed38e
Xavier Escobar <xavier_18_96@hotmail.com>
2015-01-23 19:21:36 +0000

    first version

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,28 @@
+//Hello world Java Program
+//
+//  first compile the program
+//      javac HelloWorld.java
+//  run the program
+//      java HelloWorld
+
+//  define a class
+public class HelloWorld{
+    
+//  add main method
+    public static void main(String[] args) {
+        
+//  print out the statement with the box around it
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2014!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("testing");
+        //test/testtest
+    }
+    
+}
+
+
